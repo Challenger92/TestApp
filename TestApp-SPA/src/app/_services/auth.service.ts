ecf0f861929d3eb7fa47fdc4e1a54dd876ac8af7
Sulaiman <sss_omari@yahoo.com>
2020-06-27 14:33:14 +0300

    End of Section 5

--- TestApp-SPA/src/app/_services/auth.service.ts
+++ TestApp-SPA/src/app/_services/auth.service.ts
@@ -10,7 +10,7 @@ export class AuthService {
 
 constructor(private http: HttpClient) { }
 
-login(model: any){
+login(model: any) {
   return this.http.post(this.baseUrl + 'login', model)
     .pipe(
       map((response: any) => {
@@ -22,7 +22,7 @@ login(model: any){
     );
 }
 
-register(model: any){
+register(model: any) {
   return this.http.post(this.baseUrl + 'register', model);
 }
 
